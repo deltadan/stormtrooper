d824305b64c0dc656db2efe41d530aa7212fd460
Dan Patrick <dan@clouddeltav.com>
2018-06-01 17:37:52 -0400

    update keys

--- docs/install-cli/install-cli.bash
+++ docs/install-cli/install-cli.bash
@@ -12,6 +12,7 @@
 ###########################################################################################
 
 #Install Azure CLI on Debian Distro
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
